11aa44af95b72f697fb645f8691ba24c9bbe2927
imoyb <imoyb@qq.com>
2025-10-01 00:02:01 +0800

    Add account page and enhance auth client with plugins for better user experience

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,11 @@
 import {createAuthClient} from "better-auth/react"
+import {deviceAuthorizationClient, lastLoginMethodClient, passkeyClient} from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
     baseURL: "http://localhost:8080/api/auth",
+    plugins: [
+        passkeyClient(),
+        deviceAuthorizationClient(),
+        lastLoginMethodClient(),
+    ]
 })
